6d3135e3551c9737371538c2d454314fb60868ca
rogeritael <rogeritael.contato@gmail.com>
2023-12-25 16:16:31 -0300

    configuração inicial do projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Main } from './src/views/Main';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      backgroundColor: '#333',
+      height: '100%',
+    }}>
+      <StatusBar style='inverted' backgroundColor='#333' />
+      <Main />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/views/Main/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "../../components/Text";
+import { MainContainer } from "./styles";
+
+export function Main(){
+    return (
+        <MainContainer>
+            <Text color="#fff">
+                App anotações
+            </Text>
+        </MainContainer>
+    )
+}
